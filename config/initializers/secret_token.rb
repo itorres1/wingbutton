3b316071a1b5aba22361c8b5c019297fec01b5ea
My Name <myemail@example.com>
2014-04-03 15:49:52 -0400

    initial commit, rspec install, updated gemfile and database yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wingbutton::Application.config.secret_key_base = '41256a48ff50da302427cd8d4422ad58569c3e44753e9a478bf691146939bb8e20bc08f2f217fe9f6fb4179e75e31ec2f8d380fd3c2fc2fefdf41878de3fe911'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wingbutton::Application.config.session_store :cookie_store, key: '_wingbutton_session'
