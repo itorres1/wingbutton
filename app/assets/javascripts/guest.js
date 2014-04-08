8fcdc91edc7ba22b973585f4d5210a60efdca7bd
My Name <myemail@example.com>
2014-04-08 16:53:12 -0400

    more work on guest js

--- app/assets/javascripts/guest.js
+++ app/assets/javascripts/guest.js
@@ -1,7 +1,8 @@
 //WingButton.fullMenu;
+// WingButton.wings = [];
+WingButton.wingTray = {};
 WingButton.wingMenu = [];
-WingButton.wings = [];
-WingButton.wingOrders = [];
+WingButton.wingOptions = [];
 
 // recursive function adds items to wingMenu list if it's a wing item
 function recursiveAddMenuItems(item) {
@@ -19,48 +20,59 @@ function recursiveAddMenuItems(item) {
   }
 }
 
-//gets restaurant items from api
+//get restaurant items from api
 $.getJSON(WingButton.currentMenuURL, function( data ) {
-  WingButton.fullMenu = data.menu;
+  // WingButton.fullMenu = data.menu;
   recursiveAddMenuItems(data.menu);
-
   WingButton.wingMenu.forEach(function(item){
-    WingButton.wings.push(new MenuItem(item.id, item.name, item.price ));
+    renderMenuItem(item);
   });
-  WingButton.wings[WingButton.wings.length-1].render()
-  //item.name  => item's or category's name
-  //item.price => 0.00 if a category or the item's price
-  //item.is_orderable => 1 if an item, 0 if not...
-  //item.id    => api's unique id
-  //item's parents' ids
-  //quantity
 });
 
-// define constructor for menu items
-function MenuItem(itemID, itemName, itemPrice){
-  this.itemID = itemID;
-  this.itemName = itemName;
-  this.itemPrice = itemPrice;
-}
-
-MenuItem.prototype.render = function(){
-  //render a menuitem
-  //<li class="wing" data-id="..."><span class="name">Hot Wing</span>...</li>
+//render function is recursive so it renders meal options
+var renderMenuItem = function(item){
   var menuSection = $('<div>');
-  menuSection.attr('class', "menu-section");
   var article = $('<article>');
-  var name = $('<h2>' + this.itemName + '</h2>')
-  var price = $('<h3>' + "$" + this.itemPrice + '</h3>')
+  var name = $('<span>' + item.name + '</span>');
+  if(item.price != '0.00'){
+    var price = $('<b>' + "$" + item.price + '</b>')
+    menuSection.attr('class', "menu-item" );
+    var input = $('<input type="text" class="item-quantity-input" id="'+item.id+'">')
+  }else{
+    menuSection.attr('class', "menu-category" );
+  };
+  var descrip = $('<p>' + item.descrip + '</p>')
+  article.append(input);
+
   article.append(name);
   article.append(price);
   menuSection.append(article);
+  $('#all-content').append(menuSection);
+  $('#'+item.id+'').keyup(function() {
+      WingButton.wingTray[item.id] = this.value;
+      renderTray();
+    });
+};
 
-  $('body').append(menuSection);
+var renderTray = function(){
+  
 }
 
-MenuItem.prototype.updateQuantity = function(quantity){
+//will be called within renderMenuItem to render options
+var renderMenuOption = function(item){
+// if (item.children) {
+  //   item.children.forEach(function(child){
+  //     renderMenuItem(child);
+  //   });
+  // } else if (item.length) {
+  //   item.forEach(function(child){
+  //     renderMenuItem(child);
+  //   });
+  // }
+};
 
-}
+
+
 
 
 // The tray is composed of menu items and optional sub-items. A single menu item's format is: [menu item id]/[qty],[option id],[option id]... Multiple menu items are joined by a +: [menu item id]/[qty]+[menu item id2]/[qty2] For example: 3270/2+3263/1,3279 Means 2 of menu item 3270 (with no sub options) and 1 of item num 3263 with sub option 3279.
