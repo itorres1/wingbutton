eed69c0b9315654f17f905ef56da310eba2bb64a
My Name <myemail@example.com>
2014-04-09 10:43:23 -0400

    ready for heroku

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -21,4 +21,30 @@ class GuestController < ApplicationController
     render :order
   end
 
+  def final
+    rid = params[:rid]
+    tray = params[:tray]
+    tip = params[:tip] 
+    delivery_date = "ASAP" 
+    first_name = params[:first_name]
+    last_name = params[:last_name] 
+    addr = params[:addr]
+    city = params[:city]
+    state = params[:state] 
+    zip = params[:zip]
+    phone = params[:phone] 
+    em = params[:em]
+    card_name = params[:card_name]
+    card_number = params[:card_number]
+    card_cvc = params[:card_cvc]
+    card_expiry = params[:card_expiry]
+    card_bill_addr = params[:card_bill_addr] 
+    card_bill_city = params[:card_bill_city] 
+    card_bill_state = params[:card_bill_state] 
+    card_bill_zip = params[:card_bill_zip] 
+    card_bill_phone = params[:card_bill_phone]
+
+    make_ordrin_guest_order(rid, tray, tip, delivery_date, first_name, last_name, addr, city, state, zip, phone, em, card_name, card_number, card_cvc, card_expiry, card_bill_addr, card_bill_city, card_bill_state, card_bill_zip, card_bill_phone)
+  end
+
 end
